1462628ee3254c950941f0f0ba653c95fd489e0e
Kunal-3004 <23je0507@iitism.ac.in>
2024-10-16 17:46:01 +0530

    Dashboard

--- app/src/main/java/com/example/farmwatch/HomeActivity.kt
+++ app/src/main/java/com/example/farmwatch/HomeActivity.kt
@@ -3,8 +3,6 @@ package com.example.farmwatch
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.farmwatch.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,6 +10,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
